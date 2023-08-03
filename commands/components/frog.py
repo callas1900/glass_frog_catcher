805c639e41e725e884f1d946299243e0bc729526
Ryo Tanaka <ryo.tanaka@yamaneco.io>
2023-08-03 23:42:29 +0900

    refactoring: print methods move to components

--- commands/components/frog.py
+++ commands/components/frog.py
@@ -1,8 +1,29 @@
 import os
 import urllib3
 import json
+import datetime 
+import dateutil.parser
 
 http = urllib3.PoolManager()
 def call(url):
     r = http.request( 'GET', url, headers={ 'Content-Type': 'application/json', 'X-Auth-Token': os.environ['GLASS_FLOG_TOKEN'] })
     return json.loads(r.data.decode('utf-8'))
+
+def print_circle(circle):
+    if len(circle['links']['roles']) > 0: 
+        print('='*20)
+        print('[', circle['name'], '](', circle['id'], ')')
+
+def print_project(count, project):
+    print('*', count, '[projects]',' <----- ' + project['description'], '[(' + project['status'] +'),' + project['created_at'] + ']')
+
+def print_oldest_projects(currents):
+    currents = sorted(currents, key=lambda d:d['created_at'])
+    rank_count = 1
+    for project in currents[:5]:
+        timestamp = dateutil.parser.parse(project['created_at'])
+        print(rank_count, '[', timestamp.strftime("%Y/%m/%d"), ']', project['description'])
+        rank_count += 1
+
+
+
--- commands/count_the_projects.py
+++ commands/count_the_projects.py
@@ -1,16 +1,5 @@
-import datetime 
-import dateutil.parser
 import components.urls as url
 import components.frog as api
-
-def print_circle(circle):
-    if len(circle['links']['roles']) > 0: 
-        print('='*20)
-        print('[', circle['name'], '](', circle['id'], ')')
-
-def print_project(count, project):
-    print('*', count, '[projects]',' <----- ' + project['description'], '[(' + project['status'] +'),' + project['created_at'] + ']')
-
 
 count = 0
 currents = []
@@ -20,11 +9,11 @@ futures = []
 
 circles = api.call(url.circles())['circles']
 for circle in circles:
-    print_circle(circle)
+    api.print_circle(circle)
     projects = api.call(url.projects().format(circle_id = circle['id']))['projects']
     for project in projects:
         count += 1
-        print_project(count, project)
+        api.print_project(count, project)
         if project['status'] == 'Current':
             currents.append(project)
         if project['status'] == 'Done':
@@ -36,12 +25,4 @@ for circle in circles:
 
 print('C:', len(currents), 'D:', len(dones), 'W:', len(waitings), 'F:', len(futures))
 # rank
-currents = sorted(currents, key=lambda d:d['created_at'])
-rank_count = 1
-for project in currents[:5]:
-    timestamp = dateutil.parser.parse(project['created_at'])
-    print(rank_count, '[', timestamp.strftime("%Y/%m/%d"), ']', project['description'])
-    rank_count += 1
-
-
-
+api.print_oldest_projects(currents)
