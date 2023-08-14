215ea0ac8d4f1c250de0270a0bb53bf58d822873
Ryo Tanaka <ryo.tanaka@yamaneco.io>
2023-08-14 09:54:50 +0900

    refactoring and turn off ranking in count project

--- commands/count_the_projects.py
+++ commands/count_the_projects.py
@@ -2,10 +2,8 @@ import components.urls as url
 import components.frog as api
 
 count = 0
+status_count = {}
 currents = []
-dones = []
-waitings = []
-futures = []
 
 circles = api.call(url.circles())['circles']
 for circle in circles:
@@ -14,15 +12,10 @@ for circle in circles:
     for project in projects:
         count += 1
         api.print_project(count, project)
-        if project['status'] == 'Current':
-            currents.append(project)
-        if project['status'] == 'Done':
-            dones.append(project)
-        if project['status'] == 'Waiting':
-            waitings.append(project)
-        if project['status'] == 'Future':
-            futures.append(project)
+        if project['status'] in status_count: 
+            status_count[project['status']] += 1
+        else:
+            status_count[project['status']] = 1
 
-print('C:', len(currents), 'D:', len(dones), 'W:', len(waitings), 'F:', len(futures))
-# rank
-api.print_oldest_projects(currents)
+print(status_count)
+#api.print_oldest_projects(currents)
